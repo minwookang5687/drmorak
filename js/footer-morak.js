73349d10189cfae06ca45b5297936125a3fafc5b
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2025-07-17 19:37:01 +0900

    Add files via upload

--- js/footer-morak.js
+++ js/footer-morak.js
@@ -5,7 +5,7 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-            대한모발건강지원
+            대한모발건강지원<br>
             리코 코퍼레이션 (C) All rights reserved <br>
             대표자: 강민우 | 개인정보보호책임자: 김선재<br>
             사업자등록번호: 366-03-03344<br>
