53290f47296de12e52ad4bcdda239282e6837a39
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2025-07-17 17:03:23 +0900

    Add files via upload

--- js/footer-morak.js
+++ js/footer-morak.js
@@ -5,7 +5,6 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-            주식회사 더모락<br>
             리코 코퍼레이션 (C) All rights reserved <br>
             대표자: 강민우 | 개인정보보호책임자: 김선재<br>
             사업자등록번호: 366-03-03344<br>
